7ca2d9b98f6628ff1ac0a868c8b26bd005de6e9c
nickrich1 <nicholas.richardson@student.fairfield.edu>
2021-06-25 10:10:25 -0400

    ctof and cnd info added

--- analysisTools/src/main/java/org/funp/particleID.java
+++ analysisTools/src/main/java/org/funp/particleID.java
@@ -23,8 +23,8 @@ public class particleID {
 
     public static Event event;
     public static PositiveEvent pos_ev ;
-    public static Particle_ID_Histo histo;
-  
+    public static Particle_ID_Histo histo_CTOF;
+    public static Particle_ID_Histo histo_CND;
     
     public static HashMap<Integer, List<Double>> runMap;
     public static double el_en_max;
@@ -36,8 +36,11 @@ public class particleID {
 
         event = new Event();
         pos_ev    = new PositiveEvent();
-        histo     = new Particle_ID_Histo();
-        histo.setOutputDir(inputParam.getOutputDir());
+        histo_CTOF     = new Particle_ID_Histo();
+        histo_CTOF.setOutputDir(inputParam.getOutputDir());
+
+        histo_CND     = new Particle_ID_Histo();
+        histo_CND.setOutputDir(inputParam.getOutputDir());
         runMap = runUtil.createMapGagikStyle();
         int index = 0;
         el_en_max=0;
@@ -102,6 +105,7 @@ public class particleID {
                     }
                 } else{
                     if (runconfig.getInt("event",0) < runMap.get(runNumber).get(1) && runconfig.getInt("event",0) > runMap.get(runNumber).get(0)){//event min and event max
+                        
                         goodEvent(particles,scint,scintExtras);
                 
                     }
@@ -118,11 +122,23 @@ public class particleID {
         
         }//end of the loop over files
 
-    TCanvas ec4 = new TCanvas("Positive Plots",1500,1500);
-    histo.drawPositives(ec4);
+    TCanvas ec4 = new TCanvas("Positive Plots CTOF",1500,1500);
+    histo_CTOF.drawPositivesCTOF(ec4);
+
+    TCanvas ec5 = new TCanvas("Positive Plots CND",1500,1500);
+    histo_CND.drawPositivesCND(ec5);
     System.out.println("Num deut evetns:" +pos_ev.getDeutEvents());
 
-    
+    System.out.println("The number of deut in CTOF before cuts:"+ pos_ev.num_deut_CTOF);
+    System.out.println("The number of deut in CTOF after cuts :"+ histo_CTOF.numDeutsCTOF);
+    System.out.println("The number of deut in CND before cuts :"+ pos_ev.num_deut_CND);
+    System.out.println("The number of deut in CND after  cuts :"+ histo_CND.numDeutsCND);
+    System.out.println("The number of pion in CTOF :"+ pos_ev.num_pion_CTOF);
+    System.out.println("The number of pion in CND  :"+ pos_ev.num_pion_CND);
+    System.out.println("The number of kaon in CTOF :"+ pos_ev.num_kaon_CTOF);
+    System.out.println("The number of kaon in CND  :"+ pos_ev.num_kaon_CND);
+    System.out.println("The number of prot in CTOF :"+ pos_ev.num_prot_CTOF);
+    System.out.println("The number of prot in CND  :"+ pos_ev.num_prot_CND);
 
 
 
@@ -143,28 +159,32 @@ public class particleID {
         if(particles.getRows()>0){
             int mostEnergeticNum = -1;
             vertexElectron = -10;
-            for(int npart=0; npart<particles.getRows(); npart++){
 
-                int pid = particles.getInt("pid", npart);
-                int status = particles.getInt("status", npart);
 
-                if(pid==11 /*&& Math.abs(status)>=2000 && Math.abs(status)<3000*/){
+            //This is to find the most energetic electron and see if it meets the criteria
+            
+            // for(int npart=0; npart<particles.getRows(); npart++){
+
+            //     int pid = particles.getInt("pid", npart);
+            //     int status = particles.getInt("status", npart);
+
+            //     if(pid==11 /*&& Math.abs(status)>=2000 && Math.abs(status)<3000*/){
                     
-                    vtmp.setPxPyPzM(particles.getFloat("px",npart),
-                    particles.getFloat("py",npart),
-                    particles.getFloat("pz",npart),
-                    0.0005);
-                    if(vtmp.e()>el_en_max){
-                        mostEnergeticNum = npart;
-                        el_en_max=vtmp.e();
-                        mostEnergetic.setPxPyPzM(particles.getFloat("px",mostEnergeticNum), particles.getFloat("py",mostEnergeticNum), particles.getFloat("pz",mostEnergeticNum), 0.0005);
-                        vertexElectron = particles.getFloat("vz", mostEnergeticNum);
-                    //this Lorentz Vector holds the most energetic of the electrons
-                    }
-                }
+            //         vtmp.setPxPyPzM(particles.getFloat("px",npart),
+            //         particles.getFloat("py",npart),
+            //         particles.getFloat("pz",npart),
+            //         0.0005);
+            //         if(vtmp.e()>el_en_max){
+            //             mostEnergeticNum = npart;
+            //             el_en_max=vtmp.e();
+            //             mostEnergetic.setPxPyPzM(particles.getFloat("px",mostEnergeticNum), particles.getFloat("py",mostEnergeticNum), particles.getFloat("pz",mostEnergeticNum), 0.0005);
+            //             vertexElectron = particles.getFloat("vz", mostEnergeticNum);
+            //         //this Lorentz Vector holds the most energetic of the electrons
+            //         }
+            //     }
 
 
-            }
+            // }
 
 
         }
@@ -183,24 +203,50 @@ public class particleID {
                 if (charge>0){
                     if(Math.abs(pid)==45){
                         // if(pos_ev.setDeut(particles,scint,scintExtras,npart)){
-                        //     histo.fillDeut(pos_ev);
+                        //     histo_CTOF.fillDeut(pos_ev);
                         // } 
                         pos_ev.setDeut(particles,scint,scintExtras,npart);
-                        
-                        histo.fillDeut(pos_ev);
+                        if (pos_ev.found_in_CTOF){
 
+                            histo_CTOF.fillDeutCTOF(pos_ev);
+                        }
+                        if (pos_ev.found_in_CND){
+                            histo_CND.fillDeutCND(pos_ev);
+                        }
+                        
                     }
                     else if(Math.abs(pid)==211){
                         pos_ev.setPion(particles,scint,scintExtras,npart);
-                        histo.fillPion(pos_ev);
+
+                        if (pos_ev.found_in_CTOF){
+                            histo_CTOF.fillPionCTOF(pos_ev);
+                        }
+                        if (pos_ev.found_in_CND){
+                            histo_CND.fillPionCND(pos_ev);
+                        }
+                        
                     }
                     else if(Math.abs(pid)==321){
                         pos_ev.setKaon(particles,scint,scintExtras,npart);
-                        histo.fillKaon(pos_ev);
+
+                        if (pos_ev.found_in_CTOF){
+                            histo_CTOF.fillKaonCTOF(pos_ev);
+                        }
+                        if (pos_ev.found_in_CND){
+                            histo_CND.fillKaonCND(pos_ev);
+                        }
+                        
                     }
                     else if(Math.abs(pid)==2212){
                         pos_ev.setProton(particles,scint,scintExtras,npart);
-                        histo.fillProt(pos_ev);
+
+                        if (pos_ev.found_in_CTOF){
+                            histo_CTOF.fillProtCTOF(pos_ev);
+                        }
+                        if (pos_ev.found_in_CND){
+                            histo_CND.fillProtCND(pos_ev);
+                        }
+                        
                     }
                 }
                
