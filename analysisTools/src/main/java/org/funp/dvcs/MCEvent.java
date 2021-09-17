7e11f1c5e691b4d49ef1b625b6e1bfa09dceca7b
nickrich1 <nicholas.richardson@student.fairfield.edu>
2021-09-17 10:18:37 -0400

    new classes made for MC

---
+++ analysisTools/src/main/java/org/funp/dvcs/MCEvent.java
@@ -0,0 +1,247 @@
+package org.funp.dvcs;
+//---- imports for HIPO4 library
+import org.jlab.jnp.hipo4.io.*;
+import org.jlab.jnp.hipo4.data.*;
+//---- imports for GROOT library
+import org.jlab.groot.data.*;
+import org.jlab.groot.graphics.*;
+//---- imports for PHYSICS library
+import org.jlab.jnp.physics.*;
+import org.jlab.jnp.reader.*;
+
+import java.util.Comparator;
+//import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import java.lang.Math;
+//import org.jlab.io.base.DataEvent;
+//import org.jlab.io.base.DataBank;
+
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
+
+
+public class MCEvent {
+    double MNUC=1.875612;
+
+    public double BeamEnergy=10.3;
+  public LorentzVector  vBeam   = new LorentzVector(0.0,0.0,BeamEnergy,BeamEnergy);
+  public LorentzVector  vTarget = new LorentzVector(0.0,0.0,0.0,MNUC);
+  public LorentzVector  velectron = new LorentzVector();
+  public LorentzVector  vphoton = new LorentzVector();
+  public LorentzVector  vhadron = new LorentzVector();
+ int PIDNUC=45;
+  int nelec_MC=0;
+  int nphot_MC=0;
+  int ndeut_MC=0;
+  int nother_MC = 0;
+  int ne_MC=-1;
+  int ng_MC=-1;
+  int nd_MC=-1;
+  double el_en_max=0;
+  double ph_en_max=0;
+  double d_en_max=0;
+
+   public MCEvent() {
+    // This constructor no parameter.
+    //System.out.println("setting the default DVCS event for hadron :" + MNUC );
+  }
+  public MCEvent(double mass) {
+    // This constructor no parameter.
+    MNUC=mass;
+    System.out.println("setting the default DVCS event for hadron :" + MNUC );
+  }
+
+  public boolean MCFilterParticles(Bank lund){
+    boolean goodEvent = false;
+    
+    nelec_MC=0;
+    nphot_MC=0;
+    ndeut_MC=0;
+    nother_MC=0;
+    ne_MC=-1;
+    ng_MC=-1;
+    nd_MC=-1;
+    el_en_max=0;
+    ph_en_max=0;
+    d_en_max=0;
+    LorentzVector  vtmp = new LorentzVector();
+    if(lund.getRows()>0){//loop over the events
+      for(int npart=0; npart<lund.getRows(); npart++){
+        int pid = lund.getInt("pid", npart);
+       // System.out.println(pid);
+        if(pid==11 /*&& Math.abs(status)>=2000 && Math.abs(status)<3000*/){
+          nelec_MC++;
+          vtmp.setPxPyPzM(lund.getFloat("px",npart),
+          lund.getFloat("py",npart),
+          lund.getFloat("pz",npart),
+          0.0005);
+          if(vtmp.e()>el_en_max){
+            ne_MC=npart;
+            el_en_max=vtmp.e();
+          }
+        }
+
+        else if(pid==22 /*&& Math.abs(status)<4000*/){
+
+          nphot_MC++;
+          vtmp.setPxPyPzM(lund.getFloat("px",npart),
+          lund.getFloat("py",npart),
+          lund.getFloat("pz",npart),
+          0.0);
+
+          if(vtmp.e()>ph_en_max){
+            ng_MC=npart;
+            ph_en_max=vtmp.e();
+            // if(status<=2000)conf=1;
+            // else if(status>=2000 && status<4000)conf=2;
+
+          }
+        }
+
+        else if(pid==1000010020 /*&& Math.abs(status)>=4000*/ ){
+                    
+            //System.out.println("hello deuteron");
+
+          
+            ndeut_MC++;
+            vtmp.setPxPyPzM(lund.getFloat("px",npart),
+            lund.getFloat("py",npart),
+            lund.getFloat("pz",npart),
+            MNUC);
+            if(vtmp.e()>d_en_max){
+              nd_MC=npart;
+              d_en_max=vtmp.e();
+            }
+        }
+        
+
+
+      }
+      if( ndeut_MC>=1 && nelec_MC>=1 && nphot_MC>=1){
+        setElectron(lund,ne_MC);
+        setHadron(lund,nd_MC);
+        setPhoton(lund,ng_MC);
+
+        //this.setHelicity(hel,runNumber);
+        goodEvent=true;
+        
+      }
+      
+    }    
+return goodEvent;
+  }
+
+   public  void setElectron(Bank lund,  int ne) {
+    velectron.setPxPyPzM(lund.getFloat("px",ne),
+    lund.getFloat("py",ne),
+    lund.getFloat("pz",ne),
+    0.0005);
+    //System.out.println(velectron.p());
+    
+  }
+
+   public  void setPhoton(Bank lund,int ng) {
+    
+
+    //System.out.println("The end lowest energy is " + initialRemainingEnergy);
+    vphoton.setPxPyPzM( lund.getFloat("px",ng),
+    lund.getFloat("py",ng),
+    lund.getFloat("pz",ng),
+    0.0);
+
+  }
+
+  public  void setHadron(Bank lund, int nh) {
+    //tmpdeut++;
+ 
+    vhadron.setPxPyPzM(lund.getFloat("px",nh),
+    lund.getFloat("py",nh),
+    lund.getFloat("pz",nh),
+    MNUC);
+    // betahad=particles.getFloat("beta",nh);
+    // chi2pidhad=particles.getFloat("chi2pid",nh);
+    
+  }
+
+  public  LorentzVector X(String listpart){
+    //System.out.println(listpart);
+    //String newlistpart=Stream.of("cda").sorted(Comparator.comparingInt(o -> Character.toLowerCase(o.charAt(0)))).collect(Collectors.joining());
+
+
+    listpart = Stream.of( listpart.split("") )
+    .sorted()
+    .collect(Collectors.joining());
+
+    //System.out.println(listpart);
+
+    LorentzVector  tmp = new LorentzVector();
+    tmp.copy(W());
+    // tmp.add(vTarget);
+    // tmp.sub(velectron);
+    if(listpart.equals("egh")){
+      tmp.sub(vphoton);
+      tmp.sub(vhadron);
+    }
+    else if(listpart.equals("eg")){
+      tmp.sub(vphoton);
+
+    }
+    else if(listpart.equals("eh")){
+      tmp.sub(vhadron);
+    }
+    else {
+      System.out.println(listpart+" combination of particle to calculate the missing particle is not supported, use e,g,h" );
+    }
+    return tmp;
+  }
+   public  LorentzVector W(){
+    LorentzVector  tmp = new LorentzVector();
+    tmp.copy(vBeam);
+    tmp.add(vTarget).sub(velectron);
+    return tmp;
+
+  }
+  public  LorentzVector Q(){
+    LorentzVector  tmp = new LorentzVector();
+    tmp.copy(vBeam);
+    tmp.sub(velectron);
+    return tmp;
+
+  }
+
+  public LorentzVector t(){
+    LorentzVector tmp = new LorentzVector();
+    tmp.copy(this.Q());
+    tmp.sub(vphoton);
+    return tmp;
+  }
+   public double Xb(){
+    return (-this.Q().mass2())/(2*0.938*(this.vBeam.e()-this.velectron.e()));
+  }
+
+  public double deltaPhiPlane(){
+    double deltaphi;
+    // LorentzVector tmp=new LorentzVector();
+    // tmp.copy(vBeam);
+    // tmp.sub(velectron);
+    Vector3 norm_Had_VPho = (vhadron.vect().cross(Q().vect()));
+    Vector3 norm_Had_Pho = (vhadron.vect().cross(vphoton.vect()));
+    deltaphi = Math.toDegrees(norm_Had_Pho.angle(norm_Had_VPho));
+    if(norm_Had_VPho.dot(vphoton.vect()) < 0 ) deltaphi = -1*deltaphi;
+    return deltaphi;
+  }
+  public double deltaPhiPlane2(){
+    double deltaphiplane;
+    Vector3 norm_Had_VPho = (vhadron.vect().cross(this.Q().vect()));
+    Vector3 norm_VPho_Pho = (this.Q().vect().cross(vphoton.vect()));
+    deltaphiplane = Math.toDegrees(norm_Had_VPho.angle(norm_VPho_Pho));
+    if(norm_Had_VPho.dot(vphoton.vect()) < 0 ) deltaphiplane = -1*deltaphiplane;
+    return deltaphiplane;
+  }
+
+}
---
+++ analysisTools/src/main/java/org/funp/dvcs/MCHisto.java
@@ -0,0 +1,271 @@
+package org.funp.dvcs;
+//---- imports for HIPO4 library
+import org.jlab.jnp.hipo4.io.*;
+import org.jlab.jnp.hipo4.data.*;
+//---- imports for GROOT library
+import org.jlab.groot.data.*;
+import org.jlab.groot.graphics.*;
+import org.jlab.groot.fitter.*;
+import org.jlab.groot.math.*;
+//---- imports for PHYSICS library
+import org.jlab.jnp.physics.*;
+import org.jlab.jnp.reader.*;
+
+import org.jlab.groot.ui.TCanvas;
+import org.jlab.groot.data.TDirectory;
+
+//import org.funp.dvcs.DvcsEvent;;
+
+public class MCHisto {
+    public H1F edgXmissingE; // missing mass of a complete DVCS final state e hadron gamma
+  public H1F edgXmissingM2 ; // missing mass of a complete DVCS final state e hadron gamma
+  public H1F edgXmissingP ; // missing mass of a complete DVCS final state e hadron gamma
+  public H1F edgXmissingPx;// missing px of a complete DVCS final state e hadron gamma
+  public H1F edgXmissingPy;// missing py of a complete DVCS final state e hadron gamma
+  public H1F edgXmissingPz;// missing pz of a complete DVCS final state e hadron gamma
+
+  public H1F edXmissingE;
+
+  public H1F edXmissingM2; // missing mass of hadron electron final state (to be compared with gamma)
+  public H1F egXmissingM2; // missing mass of gamma electron final state (to be compared with hadron)
+  public H1F egXmissingM;
+  public H1F edXmissingM;
+  public H2F egXmissingM2vsTh;
+
+  public H1F MomentumX_elec;
+  public H1F MomentumY_elec;
+  public H1F MomentumZ_elec;
+
+  public H1F MomentumX_phot;
+  public H1F MomentumY_phot;
+  public H1F MomentumZ_phot;
+
+  public H1F MomentumX_deut;
+  public H1F MomentumY_deut;
+  public H1F MomentumZ_deut;
+
+  public H2F WvsQ2;
+  public H2F Q2vsXbj;
+
+  public H1F W; //invariant mass of e target -> e' X
+  public H1F Q2;//Momentum transfer squared  of e-e'
+
+  public H2F tvsq2;
+  public H1F thisto;
+
+public H1F DeltaPhiPlaneHist; //angle planes Q2/hadron and gamma/hadrom
+  public H1F DeltaPhiPlaneMattHist;//angle planes Q2/hadron and Q2/gamma
+
+  public String outputdir=new String(".");//changed to public for now
+  public void setOutputDir(String otherdir){
+    
+    this.outputdir=otherdir;
+    System.out.println("**** setting out dir for plots to" + this.outputdir);
+  }
+
+  public MCHisto() {
+
+      MomentumX_elec = new H1F("momentum x electron",100,-3,3);
+      MomentumX_elec.setTitleX("momentum x electron");
+      MomentumY_elec = new H1F("momentum y electron",100,-3,3);
+      MomentumY_elec.setTitleX("momentum y electron");
+      MomentumZ_elec = new H1F("momentum z electron",100,-3,10);
+      MomentumZ_elec.setTitleX("momentum Z electron");
+
+       MomentumX_phot = new H1F("momentum x photon",100,-3,3);
+      MomentumX_phot.setTitleX("momentum x photon");
+      MomentumY_phot = new H1F("momentum y photon",100,-3,3);
+      MomentumY_phot.setTitleX("momentum y photon");
+      MomentumZ_phot = new H1F("momentum z photon",100,-3,10);
+      MomentumZ_phot.setTitleX("momentum Z photon");
+
+   MomentumX_deut = new H1F("momentum x deuteron",100,-3,3);
+      MomentumX_deut.setTitleX("momentum x deuteron");
+      MomentumY_deut = new H1F("momentum y deuteron",100,-3,3);
+      MomentumY_deut.setTitleX("momentum y deuteron");
+      MomentumZ_deut = new H1F("momentum z deuteron",100,-3,3);
+      MomentumZ_deut.setTitleX("momentum Z deuteron");
+
+
+      edgXmissingE = new H1F("edgXmissingE",100,0,10);
+    edgXmissingE.setTitleX("eDγX Missing Energy [GeV]");
+    edXmissingE = new H1F ("edXmissingE",100,-5,5);
+    edXmissingE.setTitleX("edXmissingE");
+    edgXmissingM2 = new H1F("edgXmissingM2",100,-4,4);
+    edgXmissingM2.setTitleX("eDγX Missing Mass^2 [GeV/c^2]^2");
+    edgXmissingP = new H1F("edgXmissingP",100,0,4);
+    edgXmissingP.setTitleX("eDγX Missing Momentum [GeV/c]");
+    edgXmissingPx = new H1F("MMomx",100,-1,1);
+    edgXmissingPx.setTitleX("Missing X Momentum");
+    edgXmissingPy = new H1F("MMomy",100,-1,1);
+    edgXmissingPy.setTitleX("Missing Y Momentum");
+    edgXmissingPz = new H1F("MMomz",100,-5,5);
+    edgXmissingPz.setTitleX("Missing Z Momentum [GeV/c]");
+
+    edXmissingM2 = new H1F("edXmissingM2",100,-10,10);
+    edXmissingM2.setTitleX("eDX Missing Mass^2 [GeV/c^2]^2");
+    egXmissingM2 = new H1F("egXmissingM2",100,-0,10);
+    egXmissingM2.setTitleX("eGammaX Missing Mass2");
+    edXmissingM = new H1F("eDXmissingM",100,-0,5);
+    edXmissingM.setTitleX("eDX Missing Mass");
+    egXmissingM = new H1F("egXmissingM",100,-0,5);
+    egXmissingM.setTitleX("M_x(ed#rarrowe#gammaX)");
+    egXmissingM2vsTh =new H2F("egXmissingM2vsTh",100,0,140,100,0,10);
+    egXmissingM2vsTh.setTitle("egXmissing M2 vs Th");
+    egXmissingM2vsTh.setTitleX("hadron theta");
+
+    
+
+    W= new H1F("W" ,100, 0, 10.0);
+    W.setTitleX("W [GeV]");
+    Q2 = new H1F("Q2",100, 0.1, 4.0);
+    Q2.setTitleX("Q^2 [GeV/c^2]^2");
+    
+
+    thisto = new H1F("-t",100,0,5);
+    thisto.setTitleX("-t [GeV/c]^2");
+
+    WvsQ2 = new H2F("W vs Q2", "W vs Q2", 100,0,7,100,0,10);
+    WvsQ2.setTitleX("W [GeV]");
+    WvsQ2.setTitleY("Q^2 [GeV/c^2]");
+    Q2vsXbj = new H2F("X_b vs Q^2","X_b vs Q^2",100,0,1,100,0,10);
+    Q2vsXbj.setTitleY("Q^2 [GeV/c^2]");
+    Q2vsXbj.setTitle("X_b");
+    tvsq2 = new H2F("t vs Q2", "t vs Q2",100,0,5,100,0,5);
+    tvsq2.setTitle("t vs Q2");
+    tvsq2.setTitleX("Q2");
+
+    DeltaPhiPlaneHist = new H1F("DeltaPhiPlane",100,-10,10);
+    DeltaPhiPlaneHist.setTitleX("Delta Phi Plane");
+    DeltaPhiPlaneMattHist = new H1F("DeltaPhiPlane",100,-100,100);
+    DeltaPhiPlaneMattHist.setTitleX("Delta Phi Plane Hattawy");
+
+  }
+
+  public void fillBasicHisto(MCEvent ev) {
+
+    W.fill(ev.W().mass());
+    Q2.fill(-ev.Q().mass2());
+    
+    WvsQ2.fill(ev.W().mass(),-ev.Q().mass2());
+    Q2vsXbj.fill(ev.Xb(),-ev.Q().mass2());
+    tvsq2.fill( -1*ev.Q().mass2(),-1*ev.t().mass2());
+thisto.fill(-1*ev.t().mass2());
+    MomentumX_elec.fill(ev.velectron.px());
+    MomentumY_elec.fill(ev.velectron.py());
+    MomentumZ_elec.fill(ev.velectron.pz());
+
+    MomentumX_phot.fill(ev.vphoton.px());
+    MomentumY_phot.fill(ev.vphoton.py());
+    MomentumZ_phot.fill(ev.vphoton.pz());
+
+    MomentumX_deut.fill(ev.vhadron.px());
+    MomentumY_deut.fill(ev.vhadron.py());
+    MomentumZ_deut.fill(ev.vhadron.pz());
+
+
+      edgXmissingE.fill(ev.X("ehg").e());
+    edgXmissingM2.fill(ev.X("ehg").mass2());
+    edgXmissingP.fill(ev.X("ehg").p());
+    edgXmissingPx.fill(ev.X("ehg").px());
+    edgXmissingPy.fill(ev.X("ehg").py());
+    edgXmissingPz.fill(ev.X("ehg").pz());
+
+    edXmissingE.fill(ev.X("eg").e());
+    edXmissingM2.fill(ev.X("eh").mass2());
+    egXmissingM2.fill(ev.X("eg").mass2());
+    edXmissingM.fill(ev.X("eh").mass());
+    egXmissingM.fill(ev.X("eg").mass());
+    egXmissingM2vsTh.fill(Math.toDegrees(ev.vhadron.theta()),ev.X("eg").mass2());
+    DeltaPhiPlaneHist.fill(ev.deltaPhiPlane());
+    DeltaPhiPlaneMattHist.fill(ev.deltaPhiPlane2());
+
+  }
+
+  public void DrawMissingQuants(TCanvas ec4){
+
+    ec4.divide(4,4);
+    String homeDir = System.getenv("HOME");
+    TDirectory dir = new TDirectory();
+    String directory = "/test";
+    dir.mkdir(directory);
+    dir.cd(directory);
+    dir.addDataSet(edXmissingE);
+    dir.writeFile("myfile.hipo");
+    //ec4.cd(0).draw(edgXmissingE);
+    ec4.cd(0).draw(edXmissingE);
+    ec4.cd(1).draw(edgXmissingM2);
+    ec4.cd(2).draw(edgXmissingP);
+   
+
+    ec4.cd(3).draw(edXmissingM);
+    ec4.cd(4).draw(edXmissingM2);
+    ec4.cd(5).draw(egXmissingM);
+    
+    ec4.cd(6).draw(DeltaPhiPlaneHist);
+    ec4.cd(7).draw(DeltaPhiPlaneMattHist);
+    ec4.cd(8).draw(edgXmissingPx);
+    ec4.cd(9).draw(edgXmissingPy);
+    ec4.cd(10).draw(edgXmissingPz);
+    ec4.cd(12).draw(egXmissingM2);
+    ec4.getCanvas().getScreenShot();
+    System.out.println(this.outputdir+"/"+ec4.getTitle()+".png" );
+    ec4.getCanvas().save(this.outputdir+"/"+ec4.getTitle()+".png");
+    //ec4.getScreenShot();
+
+
+
+  }
+
+  public void DrawMomentums(TCanvas ec4){
+
+    ec4.divide(3,3);
+   
+    //ec4.cd(0).draw(edgXmissingE);
+    ec4.cd(0).draw(MomentumX_elec);
+    ec4.cd(1).draw(MomentumY_elec);
+    ec4.cd(2).draw(MomentumZ_elec);
+   
+
+    ec4.cd(3).draw(MomentumX_phot);
+    ec4.cd(4).draw(MomentumY_phot);
+    ec4.cd(5).draw(MomentumZ_phot);
+
+    ec4.cd(6).draw(MomentumX_deut);
+    ec4.cd(7).draw(MomentumY_deut);
+    ec4.cd(8).draw(MomentumZ_deut);
+  
+    ec4.getCanvas().getScreenShot();
+    System.out.println(this.outputdir+"/"+ec4.getTitle()+".png" );
+    ec4.getCanvas().save(this.outputdir+"/"+ec4.getTitle()+".png");
+    //ec4.getScreenShot();
+
+
+
+  }
+
+  public void DrawKinematics(TCanvas ec4){
+
+    ec4.divide(3,2);
+   
+    //ec4.cd(0).draw(edgXmissingE);
+    ec4.cd(0).draw(W);
+    ec4.cd(1).draw(Q2);
+    ec4.cd(2).draw(thisto);
+    ec4.cd(3).draw(WvsQ2);
+   
+
+    ec4.cd(4).draw(Q2vsXbj);
+    ec4.cd(5).draw(tvsq2);
+   
+    ec4.getCanvas().getScreenShot();
+    System.out.println(this.outputdir+"/"+ec4.getTitle()+".png" );
+    ec4.getCanvas().save(this.outputdir+"/"+ec4.getTitle()+".png");
+    //ec4.getScreenShot();
+
+
+
+  }
+
+
+}
